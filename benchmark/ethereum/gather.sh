95d75e11115be48b31d476e8574df84c2a00aeb3
senju-dev <diablo_soccer@hotmail.com>
2019-08-24 13:54:48 +0200

    fix on quotations and removed comments

--- benchmark/ethereum/gather.sh
+++ benchmark/ethereum/gather.sh
@@ -9,7 +9,7 @@ for host in `cat $HOSTS`; do
   if [[ $i -lt $1 ]]; then
     echo "adding peer to network, rpcport $RPCPORT"
     port=`expr $PORT_INIT + $i`
-    echo "admin.addPeer("`ssh -i ~/.ssh/JDev.pem $USER@$host $ETH_HOME/enode.sh $host $port 2>/dev/null | grep enode`")" >> addPeer.txt
+    echo "admin.addPeer(\"`ssh -i ~/.ssh/JDev.pem $USER@$host $ETH_HOME/enode.sh $host $port 2>/dev/null | grep enode`\")" >> addPeer.txt
   fi
   let i=$i+1
   echo $i

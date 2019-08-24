4bec4be05b0fd77bbae32c499dba0109fbdb2142
senju-dev <diablo_soccer@hotmail.com>
2019-08-24 13:39:45 +0200

    fixed gathering peer information

--- benchmark/ethereum/enode.sh
+++ benchmark/ethereum/enode.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 cd `dirname ${BASH_SOURCE-$0}`
 . env.sh
-ip_addr=$1
 
-echo Running: geth --datadir=$ETH_DATA --rpc --rpcaddr 0.0.0.0 --rpcport $RPCPORT --rpccorsdomain "*" --gasprice 0 --networkid 9119 --unlock 0 --password <(echo -n "${PWD}") js <(echo 'console.log(admin.nodeInfo.enode);') 2>/dev/null |grep enode | perl -pe "s/\[\:\:\]/$ip_addr/g" | perl -pe "s/^/\"/; s/\s*$/\"/;"
-geth --datadir=$ETH_DATA --rpc --rpcaddr 0.0.0.0 --rpcport $RPCPORT --rpccorsdomain "*" --gasprice 0 --networkid 9119 --unlock 0 --allow-insecure-unlock --password <(echo -n "${PWD}") js <(echo 'console.log(admin.nodeInfo.enode);') 2>/dev/null | grep enode | sed -e "s/@127.0.0.1/@$ip_addr/"
+ip_addr=$1
+port=$2
+echo Running: geth --datadir=$ETH_DATA --rpc --rpcaddr 0.0.0.0 --rpcport $RPCPORT --rpccorsdomain "*" --gasprice 0 --networkid 9119 --unlock 0 --password <(echo -n "${PWD}") js <(echo 'console.log(admin.nodeInfo.enode);') 2>/dev/null |grep enode | perl -pe "s/\[\:\:\]/$ip_addr:$port/g" | perl -pe "s/^/\"/; s/\s*$/\"/;"
+geth --datadir=$ETH_DATA --rpc --rpcaddr 0.0.0.0 --rpcport $RPCPORT --rpccorsdomain "*" --gasprice 0 --networkid 9119 --unlock 0 --allow-insecure-unlock --password <(echo -n "${PWD}") js <(echo 'console.log(admin.nodeInfo.enode);') 2>/dev/null | grep enode | sed -e "s/@127.0.0.1:30303/@$ip_addr:$port/"
+#echo Running: geth --datadir=$ETH_DATA --gasprice 0 --networkid 9119 --unlock 0 --allow-insecure-unlock --password <(echo -n "${PWD}") js <(echo 'console.log(admin.nodeInfo.enode);') 2>/dev/null | grep enode | sed -e "s/@127.0.0.1:30303/@$ip_addr/"
+#geth --datadir=$ETH_DATA --gasprice 0 --networkid 9119 --unlock 0 --allow-insecure-unlock --password <(echo -n "${PWD}") js <(echo 'console.log(admin.nodeInfo.enode);') 2>/dev/null | grep enode | sed -e "s/@127.0.0.1:30303/@$ip_addr/"
--- benchmark/ethereum/gather.sh
+++ benchmark/ethereum/gather.sh
@@ -8,7 +8,8 @@ i=0
 for host in `cat $HOSTS`; do
   if [[ $i -lt $1 ]]; then
     echo "adding peer to network, rpcport $RPCPORT"
-    echo "admin.addPeer("`ssh -i ~/.ssh/JDev.pem $USER@$host $ETH_HOME/enode.sh $host 2>/dev/null | grep enode`")" >> addPeer.txt
+    port=`expr $PORT_INIT + $i`
+    echo "admin.addPeer("`ssh -i ~/.ssh/JDev.pem $USER@$host $ETH_HOME/enode.sh $host $port 2>/dev/null | grep enode`")" >> addPeer.txt
   fi
   let i=$i+1
   echo $i

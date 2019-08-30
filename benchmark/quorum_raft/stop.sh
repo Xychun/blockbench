eec7c210b34512287ca93c1c063ca0c7a0e4d84e
senju-dev <diablo_soccer@hotmail.com>
2019-08-30 21:39:49 +0200

    added sftp automation

--- benchmark/quorum_raft/stop.sh
+++ benchmark/quorum_raft/stop.sh
@@ -4,6 +4,6 @@ cd `dirname ${BASH_SOURCE-$0}`
 
 echo "stop.sh" 
 killall -KILL geth ${QUORUM}
-rm -rf $QUO_DATA
+sudo rm -rf $QUO_DATA
 #rm -rf $QUO_DATA/chaindata/
-rm -rf ~/.eth*
+sudo rm -rf ~/.eth*
